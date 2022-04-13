deebccd6dacddce2854a2d7080751f875b875a69
Caio Alencar <caio.alencar@icolabora.com>
2022-04-13 19:56:06 -0300

    dao de mensagens criada

---
+++ backend/dao/daoMensagens.js
@@ -0,0 +1,129 @@
+const {pool} = require('../postgres')
+const format = require('pg-format')
+
+class DaoGrupos {
+  constructor () {
+    this.tabela = 'grupos'
+    this.bd = pool
+  }
+  createTable() {
+    return new Promise( async (resolve, reject) => {
+      try {
+        const { rows } = await this.bd.query(`
+          CREATE TABLE IF NOT EXISTS ${process.env.DB_SCHEMA}.${this.tabela} (
+            id SERIAL PRIMARY KEY,
+            conteudo TEXT NOT NULL,
+            enviado_em TIMESTAMP NOT NULL,
+            editado_em TIMESTAMP NOT NULL,
+            grupo_id INT NOT NULL,
+            enviado_por INT NOT NULL,
+            CONSTRAINT fk_grupo_id
+              FOREIGN KEY(grupo_id)
+                REFERENCES grupos(id)
+            CONSTRAINT fk_enviado_por
+              FOREIGN KEY(enviado_por)
+                REFERENCES usuarios(id)
+          );
+        `)
+        resolve(rows[0])
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+  create (mensagem) {
+    return new Promise( async (resolve, reject) => {
+      try {
+        const { rows } = await this.bd.query(`
+          INSERT INTO ${process.env.DB_SCHEMA}.${this.tabela} (conteudo, enviado_em, editado_em, grupo_id, enviado_por)
+          VALUES ($1, $2, $3, $4, $5)
+          ON CONFLICT DO NOTHING
+          RETURNING *;
+        `, [mensagem.conteudo, mensagem.enviado_em, mensagem.editado_em, mensagem.grupo_id, mensagem.enviado_por])
+        resolve(rows[0])
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+  findBy (filtros) {
+    return new Promise( async (resolve, reject) => {
+      let binds = []
+      let contador = 1
+      Object.keys(filtros).forEach(key => {
+        let sqlParcial = `%I = `
+        sqlParcial = format(sqlParcial, key)
+        binds.push(sqlParcial)
+      })
+      binds = binds.map(item => {
+        const condicional = item+`$${contador}`
+        contador++
+        return condicional
+      })
+      try {
+        const { rows } = await this.bd.query(`SELECT * FROM ${process.env.DB_SCHEMA}.${this.tabela} WHERE ${binds.join(' AND ')};`, [...Object.values(filtros)])
+        resolve(rows)
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+  update (filtro, dados) {
+    return new Promise( async (resolve, reject) => {
+      if(!filtro){
+        reject(new Error('Filtro não informado'))
+      }
+      let bindsDados = []
+      let contador = 1
+      Object.keys(dados).forEach(key => {
+        let sqlParcial = `%I = `
+        sqlParcial = format(sqlParcial, key)
+        bindsDados.push(sqlParcial)
+      })
+      bindsDados = bindsDados.map(item => {
+        const condicional = item+`$${contador}`
+        contador++
+        return condicional
+      })
+      let bindsFiltros = []
+      Object.keys(filtro).forEach(key => {
+        let sqlParcial = `%I = `
+        sqlParcial = format(sqlParcial, key)
+        bindsFiltros.push(sqlParcial)
+      })
+      bindsFiltros = bindsFiltros.map(item => {
+        const condicional = item+`$${contador}`
+        contador++
+        return condicional
+      })
+      try {
+        const { rows } = await this.bd.query(`
+          UPDATE ${process.env.DB_SCHEMA}.${this.tabela}
+          SET ${bindsDados.join(', ')}
+          WHERE ${bindsFiltros.join(' AND ')}
+          RETURNING *;
+        `, [...Object.values(dados), ...Object.values(filtro)])
+        resolve(rows)
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+  delete (id) {
+    return new Promise( async (resolve, reject) => {
+      if(!id){
+        reject(new Error('Filtro não informado'))
+      }
+      try {
+        const { rows } = await this.bd.query(`
+         DELETE FROM ${process.env.DB_SCHEMA}.${this.tabela}
+         WHERE CODIGO = $1
+        `, [id])
+        resolve(rows[0])
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+}
+module.exports = {DaoTurmas}
