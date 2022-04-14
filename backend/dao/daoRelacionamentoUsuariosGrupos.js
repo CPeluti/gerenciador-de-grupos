7d1cc76e596676b6a60c6d42471ac442b6944903
Caio Alencar <caio.alencar@icolabora.com>
2022-04-13 21:10:34 -0300

    tabelas de relacionamento adicionadas

---
+++ backend/dao/daoRelacionamentoUsuariosGrupos.js
@@ -0,0 +1,126 @@
+const {pool} = require('../postgres')
+const format = require('pg-format')
+
+class DaoRelacionamentoTurmasParticipantes {
+  constructor () {
+    this.tabela = 'turmas_participantes'
+    this.bd = pool
+  }
+  createTable() {
+    return new Promise( async (resolve, reject) => {
+      try {
+        const { rows } = await this.bd.query(`
+          CREATE TABLE IF NOT EXISTS ${process.env.DB_SCHEMA}.${this.tabela} (
+            id SERIAL PRIMARY KEY,
+            id_usuario INT NOT NULL,
+            id_grupo INT NOT NULL,
+            CONSTRAINT fk_id_usuario
+              FOREIGN KEY(id_usuario)
+                REFERENCES usuarios(id)
+            CONSTRAINT fk_id_grupo
+              FOREIGN KEY(id_grupo)
+                REFERENCES grupo(id)
+          );
+        `)
+        resolve(rows[0])
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+  create (relacionamento) {
+    return new Promise( async (resolve, reject) => {
+      try {
+        const { rows } = await this.bd.query(`
+          INSERT INTO ${process.env.DB_SCHEMA}.${this.tabela} (id_usuario, id_grupo)
+          VALUES ($1, $2)
+          ON CONFLICT DO NOTHING
+          RETURNING *;
+        `, [relacionamento.id_usuario, relacionamento.id_grupo])
+        resolve(rows[0])
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+  findBy (filtros) {
+    return new Promise( async (resolve, reject) => {
+      let binds = []
+      let contador = 1
+      Object.keys(filtros).forEach(key => {
+        let sqlParcial = `%I = `
+        sqlParcial = format(sqlParcial, key)
+        binds.push(sqlParcial)
+      })
+      binds = binds.map(item => {
+        const condicional = item+`$${contador}`
+        contador++
+        return condicional
+      })
+      try {
+        const { rows } = await this.bd.query(`SELECT * FROM ${process.env.DB_SCHEMA}.${this.tabela} WHERE ${binds.join(' AND ')};`, [...Object.values(filtros)])
+        resolve(rows)
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+  update (filtro, dados) {
+    return new Promise( async (resolve, reject) => {
+      if(!filtro){
+        reject(new Error('Filtro não informado'))
+      }
+      let bindsDados = []
+      let contador = 1
+      Object.keys(dados).forEach(key => {
+        let sqlParcial = `%I = `
+        sqlParcial = format(sqlParcial, key)
+        bindsDados.push(sqlParcial)
+      })
+      bindsDados = bindsDados.map(item => {
+        const condicional = item+`$${contador}`
+        contador++
+        return condicional
+      })
+      let bindsFiltros = []
+      Object.keys(filtro).forEach(key => {
+        let sqlParcial = `%I = `
+        sqlParcial = format(sqlParcial, key)
+        bindsFiltros.push(sqlParcial)
+      })
+      bindsFiltros = bindsFiltros.map(item => {
+        const condicional = item+`$${contador}`
+        contador++
+        return condicional
+      })
+      try {
+        const { rows } = await this.bd.query(`
+          UPDATE ${process.env.DB_SCHEMA}.${this.tabela}
+          SET ${bindsDados.join(', ')}
+          WHERE ${bindsFiltros.join(' AND ')}
+          RETURNING *;
+        `, [...Object.values(dados), ...Object.values(filtro)])
+        resolve(rows)
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+  delete (id) {
+    return new Promise( async (resolve, reject) => {
+      if(!id){
+        reject(new Error('Filtro não informado'))
+      }
+      try {
+        const { rows } = await this.bd.query(`
+         DELETE FROM ${process.env.DB_SCHEMA}.${this.tabela}
+         WHERE CODIGO = $1
+        `, [id])
+        resolve(rows[0])
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+}
+module.exports = {DaoRelacionamentoTurmasParticipantes}
